aabe01bacb938e4d7c82841db45bf91ae6d9cec0
aidenLloydK <llcwispr@gmail.com>
2025-03-25 22:30:31 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
 let money = parseInt(localStorage.getItem("money")) || 0;
-
-
 let workers = parseInt(localStorage.getItem("workers")) || 0;
 let workerCost = parseInt(localStorage.getItem("workerCost")) || 25;
 let workerIncome = 1;
-
 
 let farm_size = parseInt(localStorage.getItem("farm_size")) || 1;
 let upgrade_cost = parseInt(localStorage.getItem("upgrade_cost")) || 500;
 let farm_size_income = 10;
 
-
 let manager = parseInt(localStorage.getItem("manager")) || 0;
 let manager_cost = parseInt(localStorage.getItem("manager_cost")) || 1000;
 let manager_income = 30;
+
+// Store interval IDs to be able to clear them later
+let workerIntervals = [];
+let farmIntervals = [];
+let managerIntervals = [];
 
 // Load stored values and update UI
 function loadGame() {
@@ -21,43 +22,40 @@ function loadGame() {
 
     // Restore passive incomes
     for (let i = 0; i < workers; i++) {
-        setInterval(() => {
+        let intervalId = setInterval(() => {
             money += workerIncome;
             updateDisplay();
             saveGame();
         }, 700);
+        workerIntervals.push(intervalId);
     }
 
     for (let i = 0; i < manager; i++) {
-        setInterval(() => {
+        let intervalId = setInterval(() => {
             money += manager_income;
             updateDisplay();
             saveGame();
         }, 700);
+        managerIntervals.push(intervalId);
     }
 
     for (let i = 1; i < farm_size; i++) {
-        setInterval(() => {
+        let intervalId = setInterval(() => {
             money += farm_size_income;
             updateDisplay();
             saveGame();
         }, 700);
+        farmIntervals.push(intervalId);
     }
 }
 
 // Save game data to localStorage
 function saveGame() {
     localStorage.setItem("money", money);
-    
-    
     localStorage.setItem("workers", workers);
     localStorage.setItem("workerCost", workerCost);
-    
-    
     localStorage.setItem("farm_size", farm_size);
     localStorage.setItem("upgrade_cost", upgrade_cost);
-    
-    
     localStorage.setItem("manager", manager);
     localStorage.setItem("manager_cost", manager_cost);
 }
@@ -77,11 +75,12 @@ function buyManager() {
         manager_cost += 150;
         updateDisplay();
         saveGame();
-        setInterval(() => {
+        let intervalId = setInterval(() => {
             money += manager_income;
             updateDisplay();
             saveGame();
         }, 700);
+        managerIntervals.push(intervalId);
     }
 }
 
@@ -93,12 +92,41 @@ function upgrade_farm() {
         upgrade_cost += 50;
         updateDisplay();
         saveGame();
-        setInterval(() => {
+        let intervalId = setInterval(() => {
             money += farm_size_income;
             updateDisplay();
             saveGame();
         }, 700);
+        farmIntervals.push(intervalId);
     }
+}
+
+// Reset game to initial state
+function reset() {
+    // Clear all intervals
+    workerIntervals.forEach(clearInterval);
+    farmIntervals.forEach(clearInterval);
+    managerIntervals.forEach(clearInterval);
+
+    // Reset game variables
+    money = 0;
+    workers = 0;
+    workerCost = 25;
+    farm_size = 1;
+    upgrade_cost = 500;
+    manager = 0;
+    manager_cost = 1000;
+
+    // Clear stored intervals arrays
+    workerIntervals = [];
+    farmIntervals = [];
+    managerIntervals = [];
+
+    // Update localStorage
+    saveGame();
+
+    // Update the display
+    updateDisplay();
 }
 
 // Buy a worker
@@ -109,11 +137,12 @@ function buyWorker() {
         workerCost += 15;
         updateDisplay();
         saveGame();
-        setInterval(() => {
+        let intervalId = setInterval(() => {
             money += workerIncome;
             updateDisplay();
             saveGame();
         }, 700);
+        workerIntervals.push(intervalId);
     }
 }
 
@@ -128,7 +157,6 @@ function updateDisplay() {
     document.getElementById("farm_price").innerText = `Upgrade Farm: $${upgrade_cost}`;
     document.getElementById("manager_price").innerText = `Hire Manager: $${manager_cost}`;
 }
-
 
 // Load the game state when the page loads
 window.onload = loadGame;
